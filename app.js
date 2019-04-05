b416a4ca022b928dda338b41c096d8f1a65d3b50
Yiqi Chang <yc2548@nyu.edu>
2019-04-05 01:31:21 -0400

    removed unused module

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var app = express();
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true}));
 
-var request = require("request")
-
 var hbs = require("hbs");
 
 app.set('view engine', 'hbs');
